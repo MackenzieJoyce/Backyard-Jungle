689eab0baeb7886c695e8d0c84f064aa0c785dc0
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-04 14:17:26 -0400

    added home route and require halpers and app engine updated file sturcture in handlebar

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const communityRoutes = require('./communitiesRoutes');
 
 router.use('/communities', communityRoutes);
+router.use('/', communityRoutes);
 
 module.exports = router;
