fbfc2ad5b227923f2827ec2ed0a9ffa1400c6ad2
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-05 11:16:16 -0400

    created route and search query for plant db

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const logFormRoutes = require('./loginFormRoute');
+const plantRoutes = require('./plantRoutes');
 
 router.use('/logform', logFormRoutes);
+router.use('/searchplant', plantRoutes);
 
 module.exports = router;
---
+++ controllers/api/plantRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Plants } = require('../../models');
+const { Op } = require("sequelize");
+
+
+//figure out in what format to pass in data and test if teh query actually works, also need || && separate properties one of the names might not match the search string
+router.post('/', async (req, res) => {
+    console.log(req.body.plant)
+    try {
+        const plantData = await Plants.findAll({
+            where: {
+                [Op.or]: [{
+                    Scientific_Name: {
+                        [Op.like]: `%${req.body.plant}`
+                    }
+                },
+                {
+                    Common_Name: {
+                        [Op.like]: `%${req.body.plant}`
+                    }
+                }
+
+                ]
+            }
+        });
+
+        // Serialize data so the template can read it
+        const plant = plantData.map((post) => post.get({ plain: true }));
+
+        // Pass serialized data into template
+        // res.render('place holder', { plant });
+        // res.end(JSON.stringify(plantData));
+        res.json(plantData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
