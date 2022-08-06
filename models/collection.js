72f083f07d17164fd3bacdc564ce9cc1e2dde61c
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-06 12:20:52 -0500

    collection model added

--- models/collection.js
+++ models/collection.js
@@ -1,40 +1,34 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection.js');
 
-class Collection extends Model {}
+class Collection extends Model { }
 
 Collection.init(
-//body, type string, allow null false
-{
-  plant_id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  description: {
-    type: DataTypes.STRING,
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'collection',
-      key: 'id',
+    //body, type string, allow null false
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        plant_id: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
     },
-  },
-},
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    timestamps: true,
-    underscored: true,
-    modelName: 'collection',
-  }
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        timestamps: true,
+        underscored: true,
+        modelName: 'collection',
+    }
 );
 
 
