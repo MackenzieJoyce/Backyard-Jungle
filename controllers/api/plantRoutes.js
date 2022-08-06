e94a58f94d8cbed94abb5cbc939bf6eee3fa902d
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-06 17:45:22 -0400

    fixed api to return multiple image URLs

--- controllers/api/plantRoutes.js
+++ controllers/api/plantRoutes.js
@@ -4,8 +4,11 @@ const { Op } = require("sequelize");
 const fetch = require('node-fetch');
 
 //options for image api
-const apiKey = 'AIzaSyAOMapkVritaEyTjG9qlmwvX5q_CjvsrX4';
-const searchEID = '41f8fc9ff288c4c86';
+// const apiKey = 'AIzaSyAOMapkVritaEyTjG9qlmwvX5q_CjvsrX4';
+// const searchEID = '41f8fc9ff288c4c86';
+
+const apiKey = 'AIzaSyCl3devJEdk4J8236Gu1jP3IsJ2MFuIvfg';
+const searchEID = '2031e8b19782f43e6';
 
 //figure out in what format to pass in data and test if teh query actually works, also need || && separate properties one of the names might not match the search string
 router.post('/', async (req, res) => {
@@ -32,28 +35,61 @@ router.post('/', async (req, res) => {
 
     // Serialize data so the template can read it
     const plants = plantData.map((plant) => plant.get({ plain: true }));
+    console.log(plants.length)
 
-    console.log(plantData);
+//**********************LETS KEEP IT HERE FOR NOW */
 
-    plants[0]['URL'] = await fetch('https://www.googleapis.com/customsearch/v1?num=1&key='
-    + apiKey
-    + '&cx='
-    + searchEID
-    + '&q='
-    + plants[0].Common_Name.split(' ').join('%')
-    + '+plant')
-    .then(response => response.json())  
-    .then(response => {
-        return response.items[0].pagemap.cse_thumbnail[0].src
+    // plants[0]['URL'] = await fetch('https://www.googleapis.com/customsearch/v1?num=1&key='
+    // + apiKey
+    // + '&cx='
+    // + searchEID
+    // + '&q='
+    // + plants[0].Common_Name.split(' ').join('%')
+    // + '+plant')
+    // .then(response => response.json())  
+    // .then(response => {
+    //     return response.items[0].pagemap.cse_thumbnail[0].src
+    // });
+
+    async function getPromise() {
+        let getUrl =[];
+        for (let i = 0; i < 2; i++) {
+            getUrl[i] = await fetch('https://www.googleapis.com/customsearch/v1?num=1&key='
+                + apiKey
+                + '&cx='
+                + searchEID
+                + '&q='
+                + plants[i].Common_Name.split(' ').join('%')
+                + '+plant')
+                .then(response => response.json())
+                .then(response => {
+                    return response.items[0].pagemap.cse_thumbnail[0].src
+                });
+        }
+        return getUrl;
+
+    }
+
+    function addUrl(item, index, arr){
+        for (var i = 0; i < 2; i++){
+            arr[index] = item[i]
+        };
+    };
+
+    getPromise().then(function (result) {
+        console.log("TEST RESULT")
+        console.log(result[1])
+        plants.forEach((element, index) => {
+            element.URL = result[index];
+          });
+        console.log(plants[0])
+        res.render('test', { layout: 'testlayout', plants });
     });
-        
-    // plants[0]['URL'] = 'https://opb-img.plantbook.io/acanthus%20ilicifolius.jpg';
+;
 
     // Pass serialized data into template
-    console.log(plants);
-    // console.log(plants[0].Common_Name.split(' ').join('%'));
     //apparantly all fo the parameters go into the same object container as layout, took me hours to figure out.
-    res.render('test', { layout: 'testlayout', plants });
+  
 
 });
 
