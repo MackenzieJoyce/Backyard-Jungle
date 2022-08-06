75d48742e0203a304a370657d52f2d61b405b960
Mackenzie <mackenziejoyce414@gmail.com>
2022-08-06 11:45:20 -0400

    trying to get the router up to view dynamic tips

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,14 +1,12 @@
-const router = require('express').Router();
-const logFormRoutes = require('./loginFormRoute');
+const router = require('express').Router()
+const logFormRoutes = require('./loginFormRoute')
 const userRoutes = require('./userRoutes')
-const plantRoutes = require('./plantRoutes');
+const plantRoutes = require('./plantRoutes')
+const profileRoutes = require('./profileRoutes')
 
-router.use('/logform', logFormRoutes);
-router.use('/searchplant', plantRoutes);
+router.use('/logform', logFormRoutes)
+router.use('/searchplant', plantRoutes)
 router.use('/', userRoutes)
+router.use('/profileRoutes', profileRoutes)
 
-
-
-
-
-module.exports = router;
+module.exports = router
--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -1,6 +1,6 @@
-const router = require('express').Router();
-const { Comment, User } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require('express').Router()
+const { Comment, User } = require('../../models')
+const withAuth = require('../../utils/auth')
 
 router.get('/', async (req, res) => {
   try {
@@ -9,23 +9,20 @@ router.get('/', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['user_name'],
-        },
-      ],
-    });
+          attributes: ['user_name']
+        }
+      ]
+    })
 
     // Serialize data so the template can read it
-    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    const comments = commentData.map((comment) => comment.get({ plain: true }))
 
     // Pass serialized data and session flag into template
-    res.render('account-dashbaord', { 
-      comments, 
-      logged_in: req.session.logged_in 
-    });
+    res.render('test', { layout: 'main' })
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 router.get('/comment/:id', async (req, res) => {
   try {
@@ -33,21 +30,21 @@ router.get('/comment/:id', async (req, res) => {
       include: [
         {
           model: User,
-          attributes: ['user_name'],
-        },
-      ],
-    });
+          attributes: ['user_name']
+        }
+      ]
+    })
 
-    const comment = commentData.get({ plain: true });
+    const comment = commentData.get({ plain: true })
 
     res.render('comment', {
       ...comment,
       logged_in: req.session.logged_in
-    });
+    })
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 // Use withAuth middleware to prevent access to route
 router.get('/profile', withAuth, async (req, res) => {
@@ -55,28 +52,28 @@ router.get('/profile', withAuth, async (req, res) => {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
       attributes: { exclude: ['password'] },
-      include: [{ model: Comment }],
-    });
+      include: [{ model: Comment }]
+    })
 
-    const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true })
 
     res.render('profile', {
       ...user,
       logged_in: true
-    });
+    })
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
-});
+})
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
-    return;
+    res.redirect('/profile')
+    return
   }
 
-  res.render('login');
-});
+  res.render('login')
+})
 
-module.exports = router;
+module.exports = router
