237c18985d675783fd5db7fa9e058f44767e2d8f
mirandamorton1 <107001559+mirandamorton1@users.noreply.github.com>
2022-08-08 14:31:19 -0500

    getting contact set up

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,15 @@
 const router = require('express').Router()
 const communityRoutes = require('./communitiesRoutes')
+const aboutRoutes = require('./aboutRoutes')
+const contactRoutes = require('./contactRoutes')
 const apiRoutes = require('./api')
 const forumRoutes = require('./forumRoutes')
 const homepageRoutes = require('./homepageRoutes')
 
 router.use('/api', apiRoutes)
 router.use('/communities', communityRoutes)
+router.use('/about', aboutRoutes)
+router.use('/contact', contactRoutes)
 router.use('/', forumRoutes)
 router.use('/', homepageRoutes)
 
