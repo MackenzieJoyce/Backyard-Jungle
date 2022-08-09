9874cec7921920b7886a55f9c09a444af306b7f8
Anastasia Tarasenko <anastas.tarasenko001@mymdc.net>
2022-08-09 13:21:49 -0400

    fixed post route

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -28,25 +28,25 @@ router.get('/', async (req, res) => {
     // Serialize data so the template can read it
     const post = postData.map((post) => post.get({ plain: true }))
 
-    // console.log(post);
+    console.log(post);
 
-    // const collectionData = await Collection.findAll({
-    //     include: Plants
-    // }
-    // )
+    const collectionData = await Collection.findAll({
+        include: Plants
+    }
+    )
 
     // Serialize data so the template can read it
-    // const collection = collectionData.map((collection) => collection.get({ plain: true }));
-    // console.log(collection[0])
-    // console.log(collection[0].plants.Scientific_Name)
-    // console.log("collection");
-    // console.log(collection);
-    // console.log(collection[0].plants.img_url);
-    // const cplants = collection[0].plants;
-    // console.log(cplants);
+    const collection = collectionData.map((collection) => collection.get({ plain: true }));
+    console.log(collection[0])
+    console.log(collection[0].plants.Scientific_Name)
+    console.log("collection");
+    console.log(collection);
+    console.log(collection[0].plants.img_url);
+    const cplants = collection[0].plants;
+    console.log(cplants);
     // Pass serialized data and session flag into template
 
-    res.render('profile-dashboard', { layout: 'main', post, ...user})
+    res.render('profile-dashboard', { layout: 'main', post, ...user, cplants})
   } catch (err) {
     res.status(500).json(err)
   }
